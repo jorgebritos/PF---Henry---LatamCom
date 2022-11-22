68eb6999def315a1d29ad94673936b996e95833c
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-22 16:44:10 -0500

    nav

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -4,8 +4,8 @@ import s from './LandingPage.module.css';
 
 export default function LandingPage() {
 	return (
-		<div className={s.backgraun}>
-			<Link to='/home'>
+		<div className={s.background}>
+			<Link className={s.Link} to='/home'>
 				<div className={s.divbtn}>
 					<button className={s.btn}>Entrar</button>
 				</div>
