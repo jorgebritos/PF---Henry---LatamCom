dd51d339047c16b9868d2cb6c9a7d24f40fcbe38
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-22 15:52:48 -0500

    styl btn

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -6,8 +6,8 @@ export default function LandingPage() {
 	return (
 		<div className={s.backgraun}>
 			<Link to='/home'>
-				<div className={s.btn}>
-					<button>Entrar</button>
+				<div className={s.divbtn}>
+					<button className={s.btn}>Entrar</button>
 				</div>
 			</Link>
 		</div>
