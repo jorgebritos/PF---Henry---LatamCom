0b0b5ecd79b5af001d5edfaa12b25f9bc76a8a10
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-23 16:41:23 -0500

    Home & links de Nav+css

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Route, useLocation } from 'react-router-dom';
 import LandingPage from './components/landing/LandingPage';
-// import HomePage from './components/home/HomePage';
+import HomePage from './components/home/HomePage';
 import NavBar from './components/NavBar/NavBar.jsx';
 
 function App() {
@@ -10,8 +10,7 @@ function App() {
 		<div className='App'>
 			{location.pathname !== '/' && <NavBar />}
 			<Route path='/' exact component={LandingPage} />
-
-			{/* <Route exact path='/home' component={HomePage} /> */}
+			<Route exact path='/home' component={HomePage} />
 		</div>
 	);
 }
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../../asset/Logo.png';
 import Carito from '../../asset/carrito.png';
 import SearchBar from '../searchBar/SearchBar.jsx';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 import s from './NavBar.module.css';
 import LoginRegister from '../Loging/LoginBar';
@@ -12,14 +12,23 @@ function NavBar() {
 		<div className={s.navBar}>
 			<nav className={s.navbar}>
 				<div className={s.logodiv}>
-					<Link>
+					<Link to={'/'}>
 						<img src={Logo} alt='Logo' height='40px' />
 					</Link>
 				</div>
-				<div className={s.options}>
-					<div className={s.navOption}>Home</div>
-					<br />
-					<div className={s.navOption}>Shop</div>
+				<div>
+					<ul className={s.ul}>
+						<li className={s.li}>
+							<Link to={'/home'} className={s.Link}>
+								<a>Home</a>
+							</Link>
+						</li>
+						<li className={s.li}>
+							<Link to={'/shop'} className={s.Link}>
+								<a>Shop</a>
+							</Link>
+						</li>
+					</ul>
 				</div>
 				<div>
 					<SearchBar />
