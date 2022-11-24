100d13c85c74cee75dfe2757857af95fa26447f9
jorgebritos <83886610+jorgebritos@users.noreply.github.com>
2022-11-24 10:57:43 -0300

    created remaining delete routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import { LoginButton } from './component/Login';
 import { LogoutButton } from './component/Logout';
-import {Profile} from './component/Profile'
+import { Profile } from './component/Profile'
 import './App.css';
 import { Route, useLocation } from 'react-router-dom';
 import LandingPage from './components/landing/LandingPage';
@@ -10,25 +10,27 @@ import LandingPage from './components/landing/LandingPage';
 import NavBar from './components/NavBar/NavBar.jsx';
 
 function App() {
-	let location = useLocation();
-	return (
-		<div className='App'>
-    <header className="App-header">
-      {isAuthenticated ? (
-         <>
-         <Profile/>
-         <LogoutButton/>
-        </>
+  const { isAuthenticated } = useAuth0
+  let location = useLocation();
+  return (
+    <div className='App'>
+      {/* <header className="App-header">
+      
+        {isAuthenticated ? (
+          <>
+            <Profile />
+            <LogoutButton />
+          </>
         ) : (
-        <LoginButton/>
+          <LoginButton />
         )}
-      </header>
-			{location.pathname !== '/' && <NavBar />}
-			<Route path='/' exact component={LandingPage} />
+      </header> */}
+      {location.pathname !== '/' && <NavBar />}
+      <Route path='/' exact component={LandingPage} />
 
-			{/* <Route exact path='/home' component={HomePage} /> */}
-		</div>
-	);
+      {/* <Route exact path='/home' component={HomePage} /> */}
+    </div>
+  );
 }
 
 export default App;
