310605e86cef629beb0d06f6343c6c31e7938a2e
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-24 08:48:13 -0500

    C de Search-css

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import s from './SearchBar.module.css';
-import { useSelector, useDispatch } from 'react-redux'
-import { searchByName } from '../../redux/actions/index'
-import './SearchBar.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { searchByName } from '../../redux/actions/index';
+// import './SearchBar.css'
 
-function SearchBar(){
-    const dispatch = useDispatch()
-    const products = useSelector((state) => state.products)
-    const [productName, setproductName] = useState('');
-    let predictionProduct=[]
-    useEffect(()=>{
-        dispatch(searchByName(productName))
-    },[productName])
+function SearchBar() {
+	const dispatch = useDispatch();
+	const products = useSelector((state) => state.products);
+	const [productName, setproductName] = useState('');
+	let predictionProduct = [];
+	useEffect(() => {
+		dispatch(searchByName(productName));
+	}, [productName]);
 
-    if(products.length > 5){
-        predictionProduct = [...products.slice(0,5)]
-    }else{
-        predictionProduct = [...products]
-    }
+	if (products.length > 5) {
+		predictionProduct = [...products.slice(0, 5)];
+	} else {
+		predictionProduct = [...products];
+	}
 	function handleSubmit(e) {
 		e.preventDefault();
 		setproductName('');
@@ -27,32 +27,39 @@ function SearchBar(){
 	function handleOnChange(e) {
 		setproductName(e.target.value);
 	}
-    console.log(products);
-    console.log(predictionProduct);
+	console.log(products);
+	console.log(predictionProduct);
 
-    return(
-        <div className='searchbar'>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div className="searchble">
-                    <input
-                        type='text'
-                        name='product'
-                        id='productsearch'
-                        value={productName}
-                        onChange={(e) => handleOnChange(e)}
-                    />
-                    {productName.length>=4?
-                    (<ul>
-                        {predictionProduct.map((sp)=> (<li key={sp.id}>{sp.name}</li>))}
-                    </ul>):
-                    <></>}
-                </div>
-                <Link to={`/SearchResults/${productName}`}>
-                    <button type='submit' className='button'>
-                        SEARCH
-                    </button>
-                </Link>
-            </form>
+	return (
+		<div>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<div className={s.content}>
+					<input
+						className={s.input}
+						type='text'
+						name='product'
+						id='productsearch'
+						value={productName}
+						onChange={(e) => handleOnChange(e)}
+					/>
+					{productName.length >= 4 ? (
+						<ul className={s.ul}>
+							{predictionProduct.map((sp) => (
+								<li className={s.li} key={sp.id}>
+									{sp.name}
+								</li>
+							))}
+						</ul>
+					) : (
+						<></>
+					)}
+					<Link className={s.Link} to={`/SearchResults/${productName}`}>
+						<button type='submit' className={s.btn}>
+							SEARCH
+						</button>
+					</Link>
+				</div>
+			</form>
 		</div>
 	);
 }
