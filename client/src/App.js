010925e51e3f2d3ee13655a31149754c36730eec
lautaro.z <lautaro.zanet@outlook.com>
2022-11-25 17:58:56 -0300

    Loging details and functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,26 @@
-import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from '@auth0/auth0-react';
+import { Profile } from './component/Profile';
 
-import { LoginButton } from './component/Login';
-import { LogoutButton } from './component/Logout';
-import {Profile} from './component/Profile'
-import './App.css';
 import { Route, useLocation } from 'react-router-dom';
+
 import LandingPage from './components/landing/LandingPage';
-// import HomePage from './components/home/HomePage';
+import HomePage from './components/home/HomePage.jsx';
 import NavBar from './components/NavBar/NavBar.jsx';
+import Product from './components/ProductDetail/Product';
 
 function App() {
 	let location = useLocation();
 	return (
 		<div className='App'>
-    <header className="App-header">
-      {isAuthenticated ? (
-         <>
-         <Profile/>
-         <LogoutButton/>
-        </>
-        ) : (
-        <LoginButton/>
-        )}
-      </header>
+
 			{location.pathname !== '/' && <NavBar />}
 			<Route path='/' exact component={LandingPage} />
+			<Route exact path='/home' component={HomePage} />
+			<Route path='/SearchResults' exact component={HomePage} />
+			<Route path='/product/:id' component={Product} />
+      <Route path='/profile' component={Profile} />
+      
 
-			{/* <Route exact path='/home' component={HomePage} /> */}
 		</div>
 	);
 }
--- client/src/components/Loging/LoginBar.jsx
+++ client/src/components/Loging/LoginBar.jsx
@@ -1,28 +1,128 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import usericon from '../../asset/usericon.png';
 import { getUser, getAllUsers } from '../../redux/actions/index';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import { UserName } from '../../component/userName';
+// import Cookies from "universal-cookie";
+import { LogoutButton } from '../../component/Logout';
+import { LoginButton } from '../../component/Login';
+import { useHistory } from "react-router-dom";
+import { Popover, Transition } from "@headlessui/react";
+import Swal from "sweetalert2"
+import s from './LoginBar.module.css';
 
 const LoginRegister = () => {
 	const dispatch = useDispatch();
 	const user = useSelector((state) => state.user);
+	const { loginWithRedirect, isAuthenticated } = useAuth0();
+	const history = useHistory();
+	// const cookies = new Cookies();
 
-	useEffect(() => {
-		dispatch(getAllUsers());
-		dispatch(getUser(1));
-	});
+	const userConfig = () => {
+        history.push("/profile");
+    };
+
+	const Logout = () => {
+		Swal.fire({
+            title: 'Sure about loging out?',
+            icon: 'warning',
+            showCancelButton: true,
+            cancelButtonColor: '#d33',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Log out!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+                
+                    // dispatch(Logout());
+                    history.push("/home");
+                } else Logout();{
+                Swal.fire(
+                    'Log out succesfully!',
+                    '',
+                    'success'
+                  )
+            }
+          })
+    };
+
+	const solutions = [
+        {
+            name: "Configuration",
+            href: userConfig,
+        },
+        {
+            name: "Log Out",
+            href: Logout,
+        },
+    ];
+
+	
+	useEffect(async () => {
+		await dispatch(getAllUsers());
+		await dispatch(getUser(1));
+	}, []);
 	return (
-		<div>
-			{user.username ? (
-				<div>
-					<img src={usericon} alt='' />
-					<h1>{user.username}</h1>
-				</div>
-			) : (
+		<div className={s.Login}>
+			{/* {user.username ? ( */}
+			<div className={s.Login}>
+				<img src={usericon} onClick={() => loginWithRedirect()} alt='' height='25px' />
+				{}
+
+				{isAuthenticated ? (
+          <>
+		  <Popover>
+             {({ open }) => (
+                     <>
+                        <Popover.Button>
+                                     <div>
+                                 <p>
+                                  {user?.name}
+                                 </p>
+                              </div>
+                      </Popover.Button>
+                         <Transition
+                                  >
+                            <Popover.Panel>
+                                 <div >
+                                     <div >
+                                         {solutions.map(
+                                             (item) => (
+                                             <div
+                                                 onClick={
+                                                         item.href
+                                                     }
+                                                >
+                                                     <div ></div>
+                                                     <div>
+                                                            <p >
+                                                              {
+                                                                item.name
+                                                             }
+                                                          </p>
+                                                   </div>
+                                                 </div>
+                                             )
+                                         )}
+                                     </div>
+                                     </div>
+                                    </Popover.Panel>
+                             </Transition>
+                            </>
+                    )}
+                    </Popover>
+				<UserName/>
+            <LogoutButton />
+          </>
+        ) : (
+          <LoginButton />
+        )}
+				<h3 className={s.h3}>{user.username}</h3>
+			</div>
+			{/* ) : (
 				'Esto no existe'
-			)}
+			)} */}
 		</div>
 	);
 };
