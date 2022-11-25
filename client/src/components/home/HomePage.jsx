35dc7e4e9a2a448a8a44f14051ac6c140d7a3c10
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-24 21:19:03 -0500

    Css home medi

--- client/src/components/home/HomePage.jsx
+++ client/src/components/home/HomePage.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getAllCategories, getAllProducts } from '../../redux/actions/index.js';
 import CardProduct from '../Card/CardProduct';
 // import Paginate from '../Paginate/Paginate';
+import s from './HomePage.module.css';
 
 export default function HomePage() {
 	const dispatch = useDispatch();
@@ -17,20 +18,22 @@ export default function HomePage() {
 	}, []);
 	console.log(products);
 	return (
-		<div>
-			<h1>Home Page</h1>
-			{products?.map((p) => {
-				return (
-					<CardProduct
-						key={p.id}
-						id={p.id}
-						name={p.name}
-						image={p.image}
-						categories={p.categories}
-						price={p.price}
-					/>
-				);
-			})}
+		<div className={s.cont}>
+			<div className={s.filter}>filtro</div>
+			<div className={s.cads}>
+				{products?.map((p) => {
+					return (
+						<CardProduct
+							key={p.id}
+							id={p.id}
+							name={p.name}
+							image={p.image}
+							categories={p.categories}
+							price={p.price}
+						/>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
