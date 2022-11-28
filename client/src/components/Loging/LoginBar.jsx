2ed8012ce9cea495904b20ef7cd863e99238be20
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-11-28 15:16:29 -0500

    loginbar

--- client/src/components/Loging/LoginBar.jsx
+++ client/src/components/Loging/LoginBar.jsx
@@ -1,135 +1,122 @@
 /* eslint-disable no-lone-blocks */
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import usericon from '../../asset/usericon.png';
-import { getUser, getAllUsers } from '../../redux/actions/index';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, } from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
+import { useSelector } from 'react-redux';
+import { useAuth0 } from '@auth0/auth0-react';
 import { UserName } from '../../components/login/userName';
-import { LogoutButton } from '../../components/login/Logout';
-import { LoginButton } from '../login/Login';
-import { useHistory, useLocation } from "react-router-dom";
-import { Popover, Transition } from "@headlessui/react";
-import Swal from "sweetalert2"
+import { useHistory } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import s from './LoginBar.module.css';
 
 const LoginRegister = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
-  const history = useHistory();
-  // const reload = document.getElementById('Log Out');
-  const location = useLocation();
+	// const dispatch = useDispatch();
+	const user = useSelector((state) => state.user);
+	const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+	const history = useHistory();
+	const [open, setOpen] = useState(false);
+	let menuRef = useRef();
 
-  const userConfig = () => {
-    history.push("/profile");
-  };
+	const userConfig = () => {
+		history.push('/profile');
+	};
 
+	const Logout = () => {
+		Swal.fire({
+			title: 'Sure about loging out?',
+			icon: 'warning',
+			showCancelButton: true,
+			cancelButtonColor: '#d33',
+			confirmButtonColor: '#3085d6',
+			confirmButtonText: 'Log out!',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				logout();
+				history.push('/home');
+				Swal.fire('Log out succesfully!', '', 'success');
+			} else {
+				history.push('/home');
+				Swal.fire('Log out canceled!', '', 'warning');
+			}
+		});
+	};
 
+	const solutions = [
+		{
+			name: 'Configuration',
+			href: userConfig,
+		},
+		{
+			name: 'Log Out',
+			href: Logout,
+		},
+	];
 
-  const Logout = () => {
-    Swal.fire({
-      title: 'Sure about loging out?',
-      icon: 'warning',
-      showCancelButton: true,
-      cancelButtonColor: '#d33',
-      confirmButtonColor: '#3085d6',
-      confirmButtonText: 'Log out!'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        logout();
-        // dispatch(Logout());
-        history.push("/home");
-        Swal.fire(
-          'Log out succesfully!',
-          '',
-          'success'
-        )
-      } else {
-        // logout();
-        history.push("/home");
-        Swal.fire(
-          'Log out canceled!',
-          '',
-          'warning'
-        )
+	useEffect(() => {
+		let handler = (e) => {
+			if (!menuRef.current.contains(e.target)) {
+				setOpen(false);
+				console.log(menuRef.current);
+			}
+		};
 
-      }
-    })
-  };
+		document.addEventListener('mousedown', handler);
 
-  const solutions = [
-    {
-      name: "Configuration",
-      href: userConfig,
-    },
-    {
-      name: "Log Out",
-      href: Logout,
-    },
-  ];
+		return () => {
+			document.removeEventListener('mousedown', handler);
+		};
+	});
 
-//	useEffect(async () => {
-//		await dispatch(getAllUsers());
-//		await dispatch(getUser(1));
-//	}, []);
+	return (
+		<div className='menu-container' ref={menuRef}>
+			<div
+				className='menu-trigger'
+				onClick={() => {
+					setOpen(!open);
+				}}>
+				<img src={usericon} alt='' height='25px' />
+			</div>
 
-  return (
-    <div className={s.Login}>
-      {/* {user.username ? ( */}
-      <div className={s.Login}>
-        <img src={usericon} alt='' height='25px' />
-        {isAuthenticated ? (
-          <>
-            <Popover>
-              {({ open }) => (
-                <>
-                  <Popover.Button>
-                    <div>
-                      <p>
-                        {user?.name}
-                      </p>
-                    </div>
-                  </Popover.Button>
-                  <Transition
-                  >
-                    <Popover.Panel>
-                      <div >
-                        <div >
-                          {solutions.map(
-                            (item) => (
-                              <div key={item.name}
-                                onClick={
-                                  item.href
-                                }
-                              >
-                                <div ></div>
-                                <div>
-                                  <p style={{ cursor: "pointer" }}>
-                                    {
-                                      item.name
-                                    }
-                                  </p>
-                                </div>
-                              </div>
-                            )
-                          )}
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover>
-            <UserName />
-          </>
-        ) : (
+			{isAuthenticated ? (
+				<div className={`dropdown-menu ${open ? 'active' : 'inactive'}`}>
+					<h3> {user?.name}</h3>
+					{solutions.map((item) => (
+						<ul key={item.name} onClick={item.href}>
+							<DropdownItem text={item.name} />
+						</ul>
+					))}
+					{/* <UserName/> */}
+				</div>
+			) : (
+				<p onClick={() => loginWithRedirect()}>Login</p>
+			)}
+			<h3 className={s.h3}>{user.username}</h3>
+		</div>
+	);
+};
+{
+	/* {isAuthenticated ? (
+      <div className={`dropdown-menu ${open? 'active' : 'inactive'}`} >
+        {user?.name}
+        {solutions.map(
+          (item) => (
+        <ul key={item.name} onClick={item.href}>
+          <DropdownItem text={item.name}/>
+        </ul>
+             ))} 
+             <UserName/> 
+            ): (
           <p onClick={() => loginWithRedirect()} style={{"cursor": "pointer"}}>Login</p>
         )}
         <h3 className={s.h3}>{user.username}</h3>
-      </div>
-    </div>
-  );
-};
+      </div> */
+}
+function DropdownItem(props) {
+	return (
+		<li className='dropdownItem'>
+			<img src={props.img}></img>
+			<a> {props.text} </a>
+		</li>
+	);
+}
 
 export default LoginRegister;
