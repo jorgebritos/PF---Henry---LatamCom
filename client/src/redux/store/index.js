5fc6801db27e9a2f0b7e4b2878f3312f2280df62
Julio Cesar <juljunior1@gmail.com>
2022-11-30 10:44:25 -0500

    Probando

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducer';
+import { combineReducers } from 'redux';
+
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
