230a671e25c562336a281581848bfe88e6cb91b4
Miguel A.Beltran <61295071+Miguelio01@users.noreply.github.com>
2022-12-03 20:46:16 -0500

    successedpayment css

--- client/src/components/CompletePayment/Counter.jsx
+++ client/src/components/CompletePayment/Counter.jsx
@@ -1,36 +1,45 @@
-import React from "react";
-import Countdown from "react-countdown"
-import s from './SuccessedPayment.module.css'
+import React from 'react';
+import Countdown from 'react-countdown';
+import s from './SuccessedPayment.module.css';
 
+const renderer = ({ seconds, completed }) => {
+	const handleClick = (e) => {
+		//e.preventDefaul()
+		window.location.href = 'http://localhost:3000/home';
+	};
+	if (completed) {
+		return (
+			<div className={s.btndiv}>
+				{' '}
+				<button onClick={(e) => handleClick(e)} className={s.homebtn}>
+					{' '}
+					Take me home
+				</button>
+			</div>
+		);
+	} else {
+		return (
+			<div className={s.cont}>
+				<p>
+					If the verifiying takes more than
+					{seconds === 1 ? ` ${seconds} second` : ` ${seconds} seconds`}, please
+					return to
+					<a href='http://localhost:3000/home' className={s.homelnk}>
+						home
+					</a>
+					page
+				</p>
+			</div>
+		);
+	}
+};
 
+const Counter = () => {
+	return (
+		<span className={s.contador}>
+			<Countdown date={Date.now() + 15000} renderer={renderer} />
+		</span>
+	);
+};
 
-const renderer=({seconds, completed})=>{
-    const handleClick=(e)=>{
-        //e.preventDefaul()
-        window.location.href = "http://localhost:3000/home"
-    }
-    if(completed){
-        return (
-            <div className={s.btndiv}> <button onClick={(e)=> handleClick(e)}className={s.homebtn}> Take me home</button></div>
-        )
-    }else{
-        return(
-            <span> If the verifiying takes more than 
-                {seconds===1?` ${seconds} second`:` ${seconds} seconds`}, please return to <a href="http://localhost:3000/home" className={s.homelnk}>home</a> page
-            </span>
-        )
-    }
-}
-
-const Counter = ()=>{
-    
-    
-
-    return(
-        <span className={s.secondMessagge}>
-            <Countdown date={Date.now()+15000} renderer={renderer}/>
-        </span>
-    )
-}
-
-export default Counter
+export default Counter;
