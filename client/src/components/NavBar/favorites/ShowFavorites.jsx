17627151faff702fe006be95df6019d1e527011e
Miguel A.Beltran <61295071+Miguelio01@users.noreply.github.com>
2022-12-05 10:58:02 -0500

    Favorito-nav

--- client/src/components/NavBar/favorites/ShowFavorites.jsx
+++ client/src/components/NavBar/favorites/ShowFavorites.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useHistory } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-import { getFavorites, removeAllFavorites, removeFavorite } from '../../../redux/actions';
+import {
+	getFavorites,
+	removeAllFavorites,
+	removeFavorite,
+} from '../../../redux/actions';
+import s from './ShowFavorites.module.css';
 
 const ShowFavorites = () => {
 	const [productsSelected, setProductsSelected] = useState([]);
-	const history = useHistory()
-	const dispatch = useDispatch()
-	let favorites = useSelector((state) => state.favorites)
+	const history = useHistory();
+	const dispatch = useDispatch();
+	let favorites = useSelector((state) => state.favorites);
 	const user = useSelector((state) => state.user);
 
 	const deleteProduct = (e, id) => {
@@ -20,34 +25,38 @@ const ShowFavorites = () => {
 
 	useEffect(() => {
 		if (user.id) {
-			dispatch(getFavorites(user.id))
+			dispatch(getFavorites(user.id));
 		}
-	}, [dispatch, favorites])
+	}, [dispatch, favorites]);
 
 	return (
-		<div>
+		<div className={s.backg}>
 			<h1>FAVORITES</h1>
 			<div>
-				<div>
+				<div className={s.conten_card}>
 					{favorites.map((producto) => {
 						return (
 							<div key={producto.id}>
-								<Link to={`/product/${producto.id}`}>
-									<div>
-										<img
-											src={producto.image}
-											width='100px'
-											alt=''
-										/>
-									</div>
-									<div>
-										<div>
-											<h4>{producto.name}</h4>
+								<Link className={s.link} to={`/product/${producto.id}`}>
+									<div className={s.card}>
+										<div className={s.cimg}>
+											<img
+												className={s.img}
+												src={producto.image}
+												width='100px'
+												alt=''
+											/>
 										</div>
-										<button
-											onClick={(e) => deleteProduct(e, producto.id)}>
-											REMOVE ITEM
-										</button>
+										<div className={s.conte2}>
+											<div>
+												<h4 className={s.nameP}>{producto.name}</h4>
+											</div>
+											<button
+												className={s.btn}
+												onClick={(e) => deleteProduct(e, producto.id)}>
+												REMOVE ITEM
+											</button>
+										</div>
 									</div>
 								</Link>
 							</div>
