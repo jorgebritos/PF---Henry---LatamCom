899144d085148080d81916d43e2c156a816cb749
Miguel A.Beltran <61295071+Miguelio01@users.noreply.github.com>
2022-12-12 14:01:13 -0500

    cambios

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ alter: false }).then(() => {
+conn.sync({ alter: true }).then(() => {
 	server.listen(port, () => {
 		console.log(`server raised in port ${port}`); // eslint-disable-line no-console
 	});
