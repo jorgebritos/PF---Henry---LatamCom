28edde5cc64e74236df6bc05de91749b04619a19
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-12-13 10:41:57 -0500

    cambiso formulari

--- client/src/components/home/filtros/FiltroCategory.jsx
+++ client/src/components/home/filtros/FiltroCategory.jsx
@@ -1,47 +1,48 @@
+export default function FiltroCategory({
+	s,
+	categories,
+	handleCategoryFilter,
+	filterCategory,
+}) {
+	return (
+		<div className={s.filtro}>
+			<h4 className={s.h4}>Filter By Category</h4>
+			<ul>
+				<li className={s.li} key={'All'}>
+					<label className={s.label}>
+						<input
+							className={s.input}
+							type={'radio'}
+							value={'All'}
+							name={'category'}
+							id='categoria'
+							onInput={(e) => handleCategoryFilter('All')}
+						/>
+						<span className={s.span}>All</span>
+					</label>
+				</li>
 
-
-export default function FiltroCategory({s, categories, handleCategoryFilter, filterCategory}){
-
-  return(
-    <div className={s.filtro}>
-						<h4 className={s.h4}>Filter By Category</h4>
-						<ul>
-
-						<li className={s.li} key={"All"}>
-										<label className={s.label}>
-											<input
-												className={s.input}
-												type={'radio'}
-												value={"All"}
-												name={'category'}
-												id='categoria'
-												onInput={(e) => handleCategoryFilter("All")}
-											/>
-											<span className={s.span}>Todos</span>
-										</label>
-									</li>
-
-							{categories?.map((c) => {
-								return (
-									<li className={s.li} key={c.name}>
-										<label className={s.label}>
-											<input
-												className={s.input}
-												type={'radio'}
-												value={c.name}
-												name={'category'}
-												id='categoria'
-												onInput={(e) => handleCategoryFilter(c.name)}
-											/>
-											<span className={s.span}>{c.name}</span>
-										</label>
-									</li>
-								);
-							})}
-						</ul>
-						<button className={s.btn} onClick={(e) => filterCategory(e)}>
-							Filter
-						</button>
-					</div>
-  )
+				{categories?.map((c) => {
+					return (
+						<li className={s.li} key={c.name}>
+							<label className={s.label}>
+								<input
+									className={s.input}
+									type={'radio'}
+									value={c.name}
+									name={'category'}
+									id='categoria'
+									onInput={(e) => handleCategoryFilter(c.name)}
+								/>
+								<span className={s.span}>{c.name}</span>
+							</label>
+						</li>
+					);
+				})}
+			</ul>
+			<button className={s.btn} onClick={(e) => filterCategory(e)}>
+				Filter
+			</button>
+		</div>
+	);
 }
