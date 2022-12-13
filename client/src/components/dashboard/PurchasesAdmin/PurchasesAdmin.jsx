8067b950e7401ce1c30e3adc3c42b17179f2cd7f
Miguel Ángel Beltrán Beltrán <miguel.angel.i@live.com>
2022-12-13 17:49:53 -0500

    cambios comet purchases

--- client/src/components/dashboard/PurchasesAdmin/PurchasesAdmin.jsx
+++ client/src/components/dashboard/PurchasesAdmin/PurchasesAdmin.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { deleteComment, dismissReport, getAllProducts, getAllPurchases, getAllReported } from '../../../redux/actions';
+import {
+	deleteComment,
+	dismissReport,
+	getAllProducts,
+	getAllPurchases,
+	getAllReported,
+} from '../../../redux/actions';
 import { Link, NavLink } from 'react-router-dom';
 import s from './PurchasesAdmin.module.css';
 
@@ -9,8 +15,9 @@ export default function PurchasesAdmin() {
 	const dispatch = useDispatch();
 	const allPurchases = useSelector((state) => state.purchasesAdmin);
 	const allProducts = useSelector((state) => state.allProducts);
-	const reportedComments = useSelector((state) => state.reportedComments)
-	const outOfStock = allProducts.length > 0 ? allProducts.filter((p) => p.stock === 0) : [];
+	const reportedComments = useSelector((state) => state.reportedComments);
+	const outOfStock =
+		allProducts.length > 0 ? allProducts.filter((p) => p.stock === 0) : [];
 
 	useEffect(() => {
 		dispatch(getAllPurchases());
@@ -32,8 +39,8 @@ export default function PurchasesAdmin() {
 	};
 
 	const dismissedReport = (id) => {
-		dispatch(dismissReport(id))
-	}
+		dispatch(dismissReport(id));
+	};
 
 	return (
 		<div className={s.conten}>
@@ -45,21 +52,46 @@ export default function PurchasesAdmin() {
 
 			<div className={s.conte_row}>
 				<div>
-					<h2>REPORTED COMMENTS</h2>
-					{reportedComments.length > 0 ? reportedComments.map((c) => {
-						return (
-							<div key={c.id}>
-								<p>User: {c.users[0].username}</p>
-								<p>Comment: {c.comment}</p>
-								<p>Inside Of: {c.products[0].name}</p>
-								<h5>Actions:</h5>
-								<button onClick={e => dispatch(deleteComment(c.users[0].id, c.products[0].id))}>Delete Comment</button> Or <button onClick={e => dismissedReport(c.id)}>Dismiss</button>
-								<br />
-								<NavLink to={`/product/${c.products[0].id}`}>Click Here to Follow the Comment</NavLink>
-							</div>
-						)
-					}) : <h3>All Good for now!</h3>}
+					<h2 className={s.h2}>REPORTED COMMENTS</h2>
+					<div className={s.cads}>
+						{reportedComments.length > 0 ? (
+							reportedComments.map((c) => {
+								return (
+									<div className={s.Card} key={c.id}>
+										<p>User: {c.users[0].username}</p>
+										<p>Comment: {c.comment}</p>
+										<p>Inside Of: {c.products[0].name}</p>
+										<h5>Actions:</h5>
+										<button
+											className={s.btn}
+											onClick={(e) =>
+												dispatch(deleteComment(c.users[0].id, c.products[0].id))
+											}>
+											Delete Comment
+										</button>{' '}
+										Or{' '}
+										<button
+											className={s.btn}
+											onClick={(e) => dismissedReport(c.id)}>
+											Dismiss
+										</button>
+										<br />
+										<NavLink
+											className={s.link}
+											to={`/product/${c.products[0].id}`}>
+											Click Here to Follow the Comment
+										</NavLink>
+									</div>
+								);
+							})
+						) : (
+							<h3>All Good for now!</h3>
+						)}
+					</div>
 				</div>
+				<br />
+				<hr className={s.hr} />
+				<br />
 				<div>
 					<h2 className={s.h2}>ITEMS OUT OF STOCK</h2>
 
@@ -92,46 +124,44 @@ export default function PurchasesAdmin() {
 					</div>
 				</div>
 				<br />
-
 				<hr className={s.hr} />
-
 				<div>
 					<h2 className={s.h2}>ALL USERS PURCHASES</h2>
 					<div>
 						{allPurchases.length > 0
 							? allPurchases.map((i) => {
-								return (
-									<div key={i.id}>
-										<p>User: {i.users[0].username}</p>
-										<h3>ITEMS:</h3>
-										<div className={s.cads}>
-											{i.products.map((p) => {
-												return (
-													<div className={s.productCard2} key={p.id}>
-														<div className={s.cimg}>
-															<img
-																className={s.img}
-																src={p.image}
-																alt={`${p.name}_image`}></img>
+									return (
+										<div key={i.id}>
+											<h3>ITEMS:</h3>
+											<p className={s.h3}>User: {i.users[0].username}</p>
+											<div className={s.cads}>
+												{i.products.map((p) => {
+													return (
+														<div className={s.productCard2} key={p.id}>
+															<div className={s.cimg}>
+																<img
+																	className={s.img}
+																	src={p.image}
+																	alt={`${p.name}_image`}></img>
+															</div>
+															<div className={s.contenE}>
+																<h4 className={s.name1}>{p.name}</h4>
+																<h5 className={s.text}>
+																	Unit Price: {p.price}
+																</h5>
+																<h6
+																	className={
+																		s.text
+																	}>{`Amount of Items Purchased: ${p.amount} item(s)`}</h6>
+															</div>
 														</div>
-														<div className={s.contenE}>
-															<h4 className={s.name1}>{p.name}</h4>
-															<h5 className={s.text}>
-																Unit Price: {p.price}
-															</h5>
-															<h6
-																className={
-																	s.text
-																}>{`Amount of Items Purchased: ${p.amount} item(s)`}</h6>
-														</div>
-													</div>
-												);
-											})}
+													);
+												})}
+											</div>
+											<h3 className={s.h3}>Total Price: ${i.totalPrice} USD</h3>
 										</div>
-										<h3 className={s.h3}>Total Price: ${i.totalPrice} USD</h3>
-									</div>
-								);
-							})
+									);
+							  })
 							: ''}
 					</div>
 				</div>
